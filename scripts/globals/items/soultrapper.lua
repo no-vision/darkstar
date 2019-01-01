3ed9f65563ef5296f83200f9642e47f8d0fca6c8
Erri <15344485+no-vision@users.noreply.github.com>
2019-01-01 13:41:33 -0600

    no message

--- scripts/globals/items/soultrapper.lua
+++ scripts/globals/items/soultrapper.lua
@@ -8,23 +8,36 @@ function onItemCheck(target)
     print("Soultrapper onItemCheck(): Soultrapper pre-use item check.")
     -- Do we have a blank soul plate in the ammo slot?
     local caster = target:getTarget()
+    if caster == nil then
+        return 1
+    end
+
     print("Soultrapper: Caster based off target:getTarget(): " .. caster:getName())
     local soulplate = caster:getStorageItem(0, 0, dsp.slot.AMMO)
+    if soulplate == nil then
+        return 1
+    end
+    
     if soulplate:getID() == 18722 or soulplate:getID() == 18725 then
         print("Soultrapper: Soulplate itemID found in ammo slot: " .. soulplate:getID() .. ", Ok to use.")
-        -- Get distance of target from caster bonus
-        local distanceBonus = (1 / caster:checkDistance(target))
-        print("Soultrapper: distanceBonus: " .. distanceBonus)
-        caster:setLocalVar("Soultrap-DistanceBonus", distanceBonus)
-        -- Get states of target, Notorious?, Using ability or spell?
-        local notoriousBonus = 0
-        if target:isMobType(MOBTYPE_NOTORIOUS) then
-            notoriousBonus = 1
+        -- Are we facing the target?
+        if caster:isFacing(target) then
+            -- Get distance of target from caster bonus
+            local distanceBonus = (1 / caster:checkDistance(target))
+            print("Soultrapper: distanceBonus: " .. distanceBonus)
+            caster:setLocalVar("Soultrap-DistanceBonus", distanceBonus)
+            -- Get states of target, Notorious?, Using ability or spell?
+            local notoriousBonus = 0
+            if target:isMobType(MOBTYPE_NOTORIOUS) then
+                notoriousBonus = 1
+            end
+            print("Soultrapper: notoriousBonus: " .. notoriousBonus)
+            caster:setLocalVar("Soultrap-NotoriousBonus", notoriousBonus)
+            -- Get target mob family attribute pool
+            target:setLocalVar("Soultrap-Caster", caster:getID())
+        else
+            print("Soultrapper: Caster is not facing the target")
         end
-        print("Soultrapper: notoriousBonus: " .. notoriousBonus)
-        caster:setLocalVar("Soultrap-NotoriousBonus", notoriousBonus)
-        -- Get target mob family attribute pool
-        target:setLocalVar("Soultrap-User", caster:getID())
         return 0
     end
     print("Soultrapper: An invalid soulplate itemId was found in the ammo slot: " .. soulplate:getItemID() .. ", Not ok to use.")
@@ -33,7 +46,7 @@ end
 
 function onItemUse(target, item)
     print("Soultrapper onItemUse(): Soultrapper is being used.")
-    local caster = target:getLocalVar("Soultrap-User")
+    local caster = target:getLocalVar("Soultrap-Caster")
     print("Soultrapper onItemUse(): Caster: " .. caster)
     print("Soultrapper onItemUse(): Target: " .. target:getID())
     if caster ~= nil then
