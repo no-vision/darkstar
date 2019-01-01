ee03e635a7ed5947037a63c408c9b06b0144c778
Erri <15344485+no-vision@users.noreply.github.com>
2019-01-01 00:57:57 -0600

    no message

---
+++ scripts/globals/items/soultrapper.lua
@@ -0,0 +1,12 @@
+-----------------------------------------
+-- ID: 18721
+-- Soultrapper
+-- Used to snap pictures of monsters
+-----------------------------------------
+
+function onItemCheck(target)
+    return 0
+end
+
+function onItemUse(target)
+end
---
+++ scripts/globals/items/soultrapper_2000.lua
@@ -0,0 +1,12 @@
+-----------------------------------------
+-- ID: 18724
+-- Soultrapper 2000
+-- Used to snap pictures of monsters
+-----------------------------------------
+
+function onItemCheck(target)
+    return 0
+end
+
+function onItemUse(target)
+end
